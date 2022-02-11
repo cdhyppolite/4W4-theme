c83bccdc214c1791825539be5a5ab0607cf5a9dd
Carl-David Hyppolite <e1975958@cmaisonneuve.qc.ca>
2022-02-11 18:42:52 -0500

    test1

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer class="site__footer">
+    <h2 class="footer__titre">Exercise 4W4</h2>
+    <h2 class="footer__copyright">&copy; <?php echo date("Y"); ?> - TIM Maisonneuve - Tous droits réservés</h2>
+</footer>
+</body>
+
+</html>
