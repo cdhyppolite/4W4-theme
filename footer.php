8ef0b5105b771a9d293fdbb81159677e3c3d0046
Carl-David Hyppolite <e1975958@cmaisonneuve.qc.ca>
2022-02-11 19:48:16 -0500

    push #2

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer class="site__footer">
-    <h2 class="footer__titre">Exercise 4W4</h2>
+    <a target="_blank" href="https://github.com/cdhyppolite/4W4-theme"><h2 class="footer__titre">Voir GitHub</h2></a>
     <h2 class="footer__copyright">&copy; <?php echo date("Y"); ?> - TIM Maisonneuve - Tous droits réservés</h2>
 </footer>
 </body>
