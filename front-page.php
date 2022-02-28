791037803e2cb5f164c06d1775cbf6dd63277438
Carl-David Hyppolite <e1975958@cmaisonneuve.qc.ca>
2022-02-28 13:50:34 -0500

    commit 5 bouton icones

--- front-page.php
+++ front-page.php
@@ -1,15 +1,6 @@
 <?php get_header() ?>
 <main class="site__main">
-
-<section class="animation">
-    <div class="animation__bloc">1</div>
-    <div class="animation__bloc">2</div>
-    <div class="animation__bloc">3</div>
-    <div class="animation__bloc">4</div>
-    <div class="animation__bloc">5</div>
-</section>
-
-    <h1>---- Front-page.php ------</h1>
+    <h1>Bienvenue sur <i><?php bloginfo('name'); ?></i> !</h1>
    <?php if (have_posts()): the_post(); ?>
         <?php the_title() ?>
         <?php the_content() ?>   
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # Exrcice #2 - 4W4 - Conception d'interface et développement Web
 ### Auteur : Carl-David Hyppolite
-### Date de remise : 25 février 2022
+### Date de remise : 2 mars 2022
 
 ```
 Prototype de site web Wordpress
