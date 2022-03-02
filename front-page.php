db977b89795dd610914bdcf745345d66e5462ad9
Carl-David Hyppolite <e1975958@cmaisonneuve.qc.ca>
2022-03-02 17:13:24 -0500

    single post Fixé

--- front-page.php
+++ front-page.php
@@ -1,6 +1,7 @@
 <?php get_header() ?>
 <main class="site__main"><!-- style="background-image: url('');" -->
      <h1>Bienvenue sur <i><?php bloginfo('name'); ?></i> !</h1>
+     <h4>Voici le dernier cours de la liste:</h4>
      <?php if (have_posts()): the_post(); ?>
      <?php the_title() ?>
      <?php the_content() ?>
--- single-post.php
+++ single-post.php
@@ -5,11 +5,13 @@
     <article class="article__seul">
         <?php the_title(); ?>
         <div class="article__seul__contenu">
+
             <!-- <?php if (has_post_thumbnail()): ?>
             <figure class="article__seul__contenu__figure">
-                <?php the_post_thumbnail("medium"); ?>
-            </figure> -->
-            <?php endif; ?>
+                <?php  the_post_thumbnail("medium"); ?>
+            </figure>
+            <?php endif; ?> -->
+
             <?php the_content(); ?>
         </div>
     </article>
