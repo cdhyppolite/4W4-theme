d1d800720c48ab654ee6c2fc35d892881d12e583
Carl-David Hyppolite <e1975958@cmaisonneuve.qc.ca>
2022-03-02 18:42:43 -0500

    Mixin personalisé

--- single-post.php
+++ single-post.php
@@ -1,10 +1,23 @@
 <?php get_header(); ?>
-
-<main class="site__main" style="background-image: url('<?= get_the_post_thumbnail_url(); ?>');">
+<?php
+    $codeCours = substr(get_the_title(), 0,7);
+    //----Afficher Bordu cours-----
+    $etat = "a-faire";
+    $reussi = array("1J1", "2J2", "3J3", "1W1","2W2","3W3","1M1","1M2","2M3","2M4", "3M5","3C1",);
+    $encours = array("4J4","4PA","4W4","4C2",);
+    if (in_array(substr($codeCours, 4,3), $reussi)) { 
+        $etat = "reussi";
+    } else if (in_array(substr($codeCours, 4,3), $encours)) { 
+        $etat = "en-cours";
+    }
+    //-----------------------------
+?>
+<main class="site__main <?= $etat; ?>" style="background-image: url('<?= get_the_post_thumbnail_url(); ?>');">
     <?php if (have_posts()): the_post(); ?>
     <article class="article__seul">
-        <?php the_title(); ?>
+        <h4><?php the_title(); ?><div class="cours_etat"></div></h4>
         <div class="article__seul__contenu">
+            
 
             <!-- <?php if (has_post_thumbnail()): ?>
             <figure class="article__seul__contenu__figure">
@@ -14,6 +27,7 @@
 
             <?php the_content(); ?>
         </div>
+        
     </article>
 
     <?php endif; ?>
