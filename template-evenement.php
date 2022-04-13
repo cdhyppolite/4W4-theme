d122b7fdf722789cd1aef864a0528465253b82c0
cdhyppolite <92059993+cdhyppolite@users.noreply.github.com>
2022-04-12 22:15:23 -0400

    background par défault si il n'y a pas d'image

--- template-evenement.php
+++ template-evenement.php
@@ -10,9 +10,16 @@
 ?>
 
 <main class="site__main">
-    <h1>------ template evenements ------</h1>
+    <!-- <h1>------ template evenements ------</h1> -->
+    <h1><?php the_title(); ?></h1>
     <?php if (have_posts()): the_post(); ?>
-    <?php the_post_thumbnail('thumbnail'); ?>
+    
+        <?php if (has_post_thumbnail()): ?>
+            <?php the_post_thumbnail('medium'); ?>
+        <?php else: ?>
+            <img src="https://media.sciencephoto.com/image/f0244454/800wm" alt="">
+        <?php endif; ?>
+
         <?php the_title(); ?>
         <?php the_content(); ?>
     <?php endif; ?>
