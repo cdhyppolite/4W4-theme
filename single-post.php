1bcb6fe3bbdae184b0b6b2afab141a069b8fadc0
cdhyppolite <92059993+cdhyppolite@users.noreply.github.com>
2022-04-12 22:26:33 -0400

    background par défault single post

--- single-post.php
+++ single-post.php
@@ -19,7 +19,7 @@
     //-----------------------------
 ?>
         <input type="checkbox" id="chk-single">
-        <main class="site__main <?= $etat; ?>" style="background-image: url('<?= get_the_post_thumbnail_url(); ?>');">
+        <main class="site__main <?= $etat; ?>" style="background-image: url('<?php if (has_post_thumbnail()) { echo get_the_post_thumbnail_url(); } else { echo "https://media.sciencephoto.com/image/f0244454/800wm"; } ?>');">
             <?php if (have_posts()): the_post(); ?>
             
             <article class="article__seul">
