34c25bf6d72ae4f57af0b3e9528dd0ad4ca50f92
cdhyppolite <92059993+cdhyppolite@users.noreply.github.com>
2022-04-13 17:01:12 -0400

    s11-c2 direction fleche avec un seul bouton de tri

--- listeCours.php
+++ listeCours.php
@@ -15,23 +15,18 @@
             $flecheAsc = '<svg width="18" height="18" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#000"> <path fill-rule="evenodd" d="M3.293 9.707a1 1 0 010-1.414l6-6a1 1 0 011.414 0l6 6a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L4.707 9.707a1 1 0 01-1.414 0z" clip-rule="evenodd"></path> </svg>';
             $flecheDesc = '<svg width="18" height="18" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" color="#000"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 14l-7 7m0 0l-7-7m7 7V3"></path> </svg>';
             
-            $nouvelleOrdreTri;
+            $nouvelleOrdreTri; $nouvelleiconeTri; $texteBoutonTri;
             if ($_GET['ordre']) {
-                if ($_GET['ordre']=='desc') 
-                    $nouvelleOrdreTri= 'asc';
-                 else 
-                    $nouvelleOrdreTri= 'desc';
-            } else 
-            $nouvelleOrdreTri= 'asc';
-            
+                if ($_GET['ordre']=='desc') { $nouvelleOrdreTri= 'asc'; $nouvelleiconeTri= $flecheAsc; $texteBoutonTri= 'Ascendant'; }
+                else { $nouvelleOrdreTri= 'desc'; $nouvelleiconeTri = $flecheDesc; $texteBoutonTri= 'Désendant'; }
+            } else { $nouvelleOrdreTri= 'asc'; $nouvelleiconeTri = $flecheAsc; $texteBoutonTri= 'Ascendant';
+            }
         ?>
 
     <?php wp_nav_menu(array( "menu" => "categorie_cours", "container" => "nav" )); ?>
 
     <nav class="boutonTri">
-        <!-- <a href="?cletri=title&ordre=asc"><?= $flecheAsc; ?>Ascendant</a> -->
-        <!-- <a href="?cletri=title&ordre=desc"><?= $flecheDesc; ?>Désendant</a> -->
-        <a href="?cletri=title&ordre=<?= $nouvelleOrdreTri; ?>"><?= $flecheDesc; ?>Inverser l'ordre</a>
+        <a href="?cletri=title&ordre=<?= $nouvelleOrdreTri; ?>"><?= $nouvelleiconeTri; ?><?= $texteBoutonTri; ?></a>
     </nav>
 
     <h2 class="formation__titre">Liste de cours -
