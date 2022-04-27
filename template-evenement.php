3b61d54f7363d5a84e58d8fabd2e03021df081ac
cdhyppolite <92059993+cdhyppolite@users.noreply.github.com>
2022-04-26 20:19:03 -0400

    s12-c2 Ajout couleur template-evenement

--- template-evenement.php
+++ template-evenement.php
@@ -9,26 +9,26 @@
     get_header();
 ?>
 
-<main class="site__main">
-     <section class="evenement">
-     <h1>---- template-evenement.php ------</h1>
-     <?php if (have_posts()):  the_post(); ?>
-     <?php the_post_thumbnail(); ?>
-     <?php the_title() ?>
-     <p class='evenement__resume'><?php the_field('resume'); ?></p>
-     <p class='evenement__endroit'><?php the_field('endroit'); ?></p> 
-     <p>Organisé par: <?php the_field('organisateur'); ?></p>
-     <p>Date: <?php the_field('date'); ?></p>
-     <p>Heure: <?php the_field('heure'); ?></p>
-     
-     <?php 
-        $image = get_field('image');
-        if( !empty( $image ) ): ?>
-        <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
-     <?php endif; ?>
+<main class="site__main" style="background-color: <?php the_field('couleur'); ?>;">
+   <section class="evenement">
+      <!-- <h1>---- template-evenement.php ------</h1> -->
+      <?php if (have_posts()):  the_post(); ?>
 
-     <?php endif ?>
-     </section>
+      <h1 class="evenement__titre"><?php the_title() ?></h1>
+
+      <?php $image = get_field('image'); ?>
+      <?php if( !empty( $image ) ): ?>
+         <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+      <?php endif; ?>
+
+      <p class='evenement__resume'><?php the_field('resume'); ?></p>
+      <p class='evenement__endroit'>Lieu: <?php the_field('endroit'); ?></p>
+      <p>Organisé par: <?php the_field('organisateur'); ?></p>
+      <p>Date: <?php the_field('date'); ?></p>
+      <p>Heure: <?php the_field('heure'); ?></p>
+
+      <?php endif ?>
+   </section>
 </main>
 
 <?php get_footer(); ?>
