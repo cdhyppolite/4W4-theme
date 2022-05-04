78b6c8428e74b14765833302e65ec2c580038001
cdhyppolite <92059993+cdhyppolite@users.noreply.github.com>
2022-05-04 19:12:52 -0400

    s14-c1 début btn suivant/précédent

--- js/carrousel.js
+++ js/carrousel.js
@@ -4,6 +4,7 @@ let modaleImg = document.querySelector('.boite__modale__img');
 let h2Modale = document.querySelector('.boite__modale__h2');
 let footerModale = document.querySelector('.boite__modale__footer');
 let navModale = document.querySelector('.boite__modale__navigation');
+let modaleBtnImgActuel;
 
 function recupererImage(event) {
     let imgCible = event.target;
@@ -19,20 +20,34 @@ function recupererImage(event) {
 function imageBtn(event) {
     let cible = event.target;
     modaleImg.setAttribute('src', ImagesGalerie[cible.dataset.indexNumber].getAttribute('src'));
+    modaleBtnImgActuel = cible.dataset.indexNumber;
+    console.log(modaleBtnImgActuel);
     // console.log(cible.dataset.indexNumber);
 }
 
 (function() {
-    // console.log(ImagesGalerie.length);
+
+    let btnImgMoins = document.createElement('button');
+    btnImgMoins.title = "Précédent";
+    btnImgMoins.classList.add('ordre');
+    navModale.append(btnImgMoins);
+
+    // Boutons pour les images
     for (let i = 0; i < ImagesGalerie.length; i++) {
         let btnImg = document.createElement('button');
         btnImg.dataset.indexNumber = i;
+        btnImg.title = "Image " + i;
         navModale.append(btnImg);
 
         btnImg.addEventListener('mousedown', function(event) {
             imageBtn(event);
         })
     }
+
+    let btnImgPlus = document.createElement('button');
+    btnImgPlus.title = "Suivant";
+    btnImgPlus.classList.add('ordre');
+    navModale.append(btnImgPlus);
 
     for (const image of ImagesGalerie) {
 
