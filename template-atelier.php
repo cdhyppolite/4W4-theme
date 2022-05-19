e5936dc9967437356e74b0071e3a477e5f1aed6b
cdhyppolite <92059993+cdhyppolite@users.noreply.github.com>
2022-05-19 13:11:28 -0400

    ef-CDH- template atelier avancé

--- template-atelier.php
+++ template-atelier.php
@@ -1,4 +1,4 @@
-<!-- Ajout du fichier template-evenement.php -->
+<!-- Ajout du fichier template-atelier.php -->
 <?php
 /**
  * Template Name: Ateliers
@@ -10,27 +10,25 @@
 ?>
 
 <main class="site__main" style="background-color: <?php the_field('couleur'); ?>;">
-   <section class="evenement">
-      <!-- <h1>---- template-evenement.php ------</h1> -->
-      <?php if (have_posts()):  the_post(); ?>
+   <section class="atelier">
 
-      <h1 class="evenement__titre">Nom de l'atelier: <?php the_title() ?></h1>
+      <h1 class="atelier__titre">Nom de l'atelier: <?php the_title() ?></h1>
 
-      <div class="evenement__contenu">
-         <h2>Description de l'atelier</h2>
+      <div class="atelier__contenu">
+         <h2 class="atelier__sous-titre">Description de l'atelier</h2>
          <p>Animé par: <?php the_field('animateur'); ?></p>
-         <p>Numéro du local: <?php the_field('local'); ?></p>
+         <p>Numéro du local: <i><?php the_field('local'); ?></i> du Collège de Maisonneuve</p>
          <!-- Description -->
          <p><?php the_field('atelier_desc'); ?></p>
 
-         <h2>Quand se déroulera l'atelier?</h2>
+         <h2 class="atelier__sous-titre">Quand se déroulera l'atelier?</h2>
          <p>Atelier commence le <?php the_field('date_debut'); ?></p>
          <p>Atelier se termine le <?php the_field('date_fin'); ?></p>
-         <p>Cette atelier formatif se donne les <?php the_field('jours'); ?></p>
+         <p>Cet atelier se donne par séance de <?php the_field('duree'); ?> heure(s)</p>
+         <p class="atelier__jours">Cette atelier formatif se donne les <i><?php the_field('jours'); ?></i></p>
          <p>L'heure: <?php the_field('heure_debut'); ?> à <?php the_field('heure_fin'); ?></p>
       </div>
 
-      <?php endif ?>
    </section>
 </main>
 
